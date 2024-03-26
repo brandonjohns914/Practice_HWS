5aa7d153e24da2e49923800db72df44db649b2cf
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-03-25 17:31:08 -0700

    cupcake corner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Brandon Johns on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
