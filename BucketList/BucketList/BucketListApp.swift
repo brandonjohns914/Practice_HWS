32c0d0c648584d547b95aa3ed5ce982e349d4825
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-03-27 17:32:17 -0700

    BucketList

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Brandon Johns on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Brandon Johns on 3/25/24.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var viewModel = ViewModel()
+    @AppStorage("mapStyle") private var mapStyle = "standard"
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    var body: some View {
+        if viewModel.isUnlocked {
+            VStack {
+                MapReader { proxy in
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            Annotation(location.name, coordinate: location.coordinate) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44, height: 44)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .onLongPressGesture {
+                                        viewModel.selectedPlace = location
+                                    }
+                            }
+                        }
+                    }
+                    .mapStyle(mapStyle == "standard" ? .standard : .hybrid)
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            viewModel.addLocation(at: coordinate)
+                        }
+                        
+                    }
+                    .sheet(item: $viewModel.selectedPlace) { place in
+                        EditView(location: place) {
+                            viewModel.update(location: $0)
+                        }
+                    }
+                }
+                Picker("Map mode", selection: $mapStyle) {
+                    Text("Standard")
+                        .tag("standard")
+
+                    Text("Hybrid")
+                        .tag("hybrid")
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+
+            }
+        } else {
+            Button("Unlock Places", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
+                .alert("Authentication error", isPresented: $viewModel.isShowingAuthenticationError) {
+                    Button("OK") { }
+                } message: {
+                    Text(viewModel.authenticationError)
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
