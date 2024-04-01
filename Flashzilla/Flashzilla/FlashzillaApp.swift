d6e8635e83b1c5bc092d3a689874e4e7703dd07c
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-04-01 14:56:42 -0700

    Flash Zilla

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Brandon Johns on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
